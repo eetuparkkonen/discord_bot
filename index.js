f77b842e448ea9070f9f1b769cfcc3e4307a98d2
eetuparkkonen <ede.parkkonen@gmail.com>
2021-03-06 22:27:51 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+bot.login(process.env.TOKEN);
+
+
+
+
+async function messageHandler(message) {
+  console.log("message ", message);
+  let response;
+  switch (message.toLowerCase()) {
+    case "moi":
+      response = "WADAP LEILEE";
+    case "juho":
+      response = "jaakkoeerikki";
+    
+      break;
+  }
+  return response;
+}
+
+bot.on("ready", () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+});
+
+bot.on("message", async (message) => {
+  try {
+    const response = await messageHandler(message.content);
+    if (response) {
+      message.reply(response);
+    }
+  } catch (error) {
+    message.reply("Virhe ", error);
+  }
+});
+
+
+  // } else if (msg.content.startsWith('!kick')) {
+  //   if (msg.mentions.users.size) {
+  //     const taggedUser = msg.mentions.users.first();
+  //     msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
+  //   } else {
+  //     msg.reply('Please tag a valid user!');
+  //   }
+  // }
+
